5b6f78ce31791ed053b5cfddf3d1571dde0466b7
Mihai Listov <mihailistov@gmail.com>
2024-03-11 10:47:08 -0700

    BUG 3531: Fix issues with node observer, release 0.8.0

--- powerpod/rollup.config.js
+++ powerpod/rollup.config.js
@@ -5,7 +5,7 @@ import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-import-css';
 
 const licenseContent = `/*!
-* powerpod 0.7.9
+* powerpod 0.8.0
 * https://github.com/bcgov/nr-af-pods/powerpod
 *
 * @license GPLv3 for open source use only
