c790e56be4d0ed9aaa1c3c17240f4c9d5389f4a2
Mihai Listov <mihailistov@gmail.com>
2024-04-22 14:49:49 -0700

    TASK 3794: NEFBA2 claim info step

--- powerpod/rollup.config.js
+++ powerpod/rollup.config.js
@@ -5,7 +5,7 @@ import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-import-css';
 
 const licenseContent = `/*!
-* powerpod 1.2.6
+* powerpod 1.2.7
 * https://github.com/bcgov/nr-af-pods/powerpod
 *
 * @license GPLv3 for open source use only
