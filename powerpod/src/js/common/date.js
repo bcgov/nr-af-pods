5c9f621914f0a1ca7f3a7fff194ae0348ea784cf
Mihai Listov <mihailistov@gmail.com>
2024-05-07 09:15:10 -0700

    TASK 3909: Minor change to time str to match Microsoft format

--- powerpod/src/js/common/date.js
+++ powerpod/src/js/common/date.js
@@ -1,9 +1,9 @@
 export function getCurrentTimeUTC() {
   const now = new Date();
-  const month = String(now.getUTCMonth() + 1).padStart(2, '0');
-  const day = String(now.getUTCDate()).padStart(2, '0');
+  const month = now.getUTCMonth() + 1;
+  const day = now.getUTCDate();
   const year = now.getUTCFullYear();
-  const hours = String(now.getUTCHours()).padStart(2, '0');
+  const hours = now.getUTCHours();
   const minutes = String(now.getUTCMinutes()).padStart(2, '0');
   const seconds = String(now.getUTCSeconds()).padStart(2, '0');
   const meridiem = hours < 12 ? 'AM' : 'PM';
