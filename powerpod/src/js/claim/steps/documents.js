aadb8e24a8e8168c2a6e4c517ffa9f35f0f81d3b
Mihai Listov <mihailistov@gmail.com>
2024-05-16 12:05:16 -0700

    TASK 3940: Add configurable doc tooltip to json, rel 1.4.1

--- powerpod/src/js/claim/steps/documents.js
+++ powerpod/src/js/claim/steps/documents.js
@@ -4,8 +4,10 @@ import { getProgramAbbreviation } from '../../common/program.ts';
 import { configureFields } from '../../common/fieldConfiguration.js';
 import { setFieldValue } from '../../common/html.js';
 import { setFieldReadOnly } from '../../common/fieldValidation.js';
-import { customizeDocumentsControls } from '../documents.js';
-import { CLAIM_FILE_UPLOAD_FIELDS } from '../../common/documents.ts';
+import { getGlobalConfigData } from '../../common/config.js';
+import { Logger } from '../../common/logger.js';
+
+const logger = Logger('claim/steps/documents');
 
 export async function customizeDocumentsStep() {
   const programAbbreviation = getProgramAbbreviation();
@@ -15,65 +17,7 @@ export async function customizeDocumentsStep() {
     programAbbreviation === 'NEFBA2' ||
     programAbbreviation.includes('KTTP')
   ) {
-    if (!document.querySelector('#supportingDocumentationNote')) {
-      const supportingDocumentationNoteHtmlContent = `
-      <style>
-        sl-tooltip::part(body) {
-          font-size: 1.2rem;
-        }
-      </style>
-      <div id="supportingDocumentationNote" style="padding-bottom: 20px;">
-        Please choose or drag & drop files to the box below to upload the following documents as attachments (as applicable).
-        <br /><br />
-        You can upload a file up to 15MB each in the 
-        <sl-tooltip>
-          <div slot="content">
-            Text/Document Files
-            <ul>
-              <li>CSV (Comma Separated Values)</li>
-              <li>DOC (Microsoft Word Document)</li>
-              <li>DOCX (Microsoft Word Open XML Document)</li>
-              <li>ODT (Open Document Text File)</li>
-              <li>PDF (Portable Document Format File)</li>
-            </ul>
-
-            Spreadsheet Files
-            <ul>
-              <li>XLS (Excel Spreadsheet)</li>
-              <li>XLSX (Microsoft Excel Open XML Spreadsheet)</li>
-              <li>ODS (Open Document Spreadsheet File)</li>
-            </ul>
-
-            Image Files
-            <ul>
-              <li>GIF (Graphical Interchange Format File)</li>
-              <li>JPEG (JPEG Image File)</li>
-              <li>JPG (JPEG Image File)</li>
-              <li>PNG (Portable Network Graphic)</li>
-              <li>SVG (Scalable Vector Graphics File)</li>
-              <li>TIF (Tagged Image File)</li>
-            </ul>
-          </div>
-          <a href="" style="font-size: 15px">supported file formats</a>.
-        </sl-tooltip>
-      </div>`;
-
-      $('fieldset[aria-label="Supporting Documents"] > legend').after(
-        supportingDocumentationNoteHtmlContent
-      );
-
-      const beforeContinuingNoteHtmlContent = `
-        <div id="beforeContinuingNote" style="padding-bottom: 20px;">
-          Please ensure you have the correct files before clicking “Next”. If you move to the next stage of the Claim for Payment form you can no longer delete uploaded files. However, you can always add new files.<br />
-          <br />
-          If you have moved to the next stage and then wish to change an uploaded file, simply return to the document submission and upload the replacement file as an additional file. Give the file a name with an indication that it is a replacement (e.g. "Budget NEW.xls").
-        </div>
-      `;
-
-      $('fieldset[aria-label="Supporting Documents"]').after(
-        beforeContinuingNoteHtmlContent
-      );
-    }
+    addDocumentsStepText();
   }
 
   // customizeDocumentsControls(CLAIM_FILE_UPLOAD_FIELDS);
@@ -91,6 +35,57 @@ export async function customizeDocumentsStep() {
 
   if (programAbbreviation === 'NEFBA2') {
     addSatisfactionSurveyChefsIframe();
+  }
+}
+
+function addDocumentsStepText() {
+  const allowedDocumentsTooltipText =
+    getGlobalConfigData()?.AllowedDocumentsTooltipText;
+
+  if (!allowedDocumentsTooltipText) {
+    logger.error({
+      fn: customizeDocumentsStep,
+      message: 'Failed to fetch AllowedDocumentsTooltipText',
+    });
+  }
+  if (!document.querySelector('#supportingDocumentationNote')) {
+    const supportingDocumentationNoteHtmlContent = `
+  <style>
+    sl-tooltip::part(body) {
+      font-size: 1.2rem;
+    }
+  </style>
+  <div id="supportingDocumentationNote" style="padding-bottom: 20px;">
+    Please choose or drag & drop files to the box below to upload the following documents as attachments (as applicable).
+    <br /><br />
+    You can upload a file up to 15MB each in the 
+    ${
+      allowedDocumentsTooltipText
+        ? `<sl-tooltip>
+        <div slot="content">
+          ${allowedDocumentsTooltipText}
+        </div>
+        <a href="" style="font-size: 15px">supported file formats</a>.
+      </sl-tooltip>`
+        : 'supported file formats.'
+    }
+  </div>`;
+
+    $('fieldset[aria-label="Supporting Documents"] > legend').after(
+      supportingDocumentationNoteHtmlContent
+    );
+
+    const beforeContinuingNoteHtmlContent = `
+    <div id="beforeContinuingNote" style="padding-bottom: 20px;">
+      Please ensure you have the correct files before clicking “Next”. If you move to the next stage of the Claim for Payment form you can no longer delete uploaded files. However, you can always add new files.<br />
+      <br />
+      If you have moved to the next stage and then wish to change an uploaded file, simply return to the document submission and upload the replacement file as an additional file. Give the file a name with an indication that it is a replacement (e.g. "Budget NEW.xls").
+    </div>
+  `;
+
+    $('fieldset[aria-label="Supporting Documents"]').after(
+      beforeContinuingNoteHtmlContent
+    );
   }
 }
 
