5d340a1c24bc86690b78cfc16b811bd9b81a0b15
Mihai Listov <mihailistov@gmail.com>
2024-05-22 13:56:27 -0700

    Fix expense report table total, save btn validation, rel 1.4.4

--- assets/claim/js/claim.js
+++ assets/claim/js/claim.js
@@ -1,6 +1,6 @@
 (function () {
   const src =
-    'https://cdn.jsdelivr.net/gh/bcgov/nr-af-pods@dev/powerpod/releases/powerpod-1.4.3.min.js';
+    'https://cdn.jsdelivr.net/gh/bcgov/nr-af-pods@dev/powerpod/releases/powerpod-1.4.4.min.js';
   const script = document.createElement('script');
   script.setAttribute('async', '');
   script.src = src;
