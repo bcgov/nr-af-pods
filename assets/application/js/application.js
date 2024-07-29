ce2a2adb0e390c6c5d742f80deff143803a4fbdb
Mihai Listov <mihailistov@gmail.com>
2024-07-29 16:57:08 -0700

    Fix generate form json fn

--- assets/application/js/application.js
+++ assets/application/js/application.js
@@ -1,6 +1,6 @@
 (function () {
   const src =
-    'https://cdn.jsdelivr.net/gh/bcgov/nr-af-pods@dev/powerpod/releases/powerpod-1.7.2.min.js';
+    'https://cdn.jsdelivr.net/gh/bcgov/nr-af-pods@dev/powerpod/releases/powerpod-1.7.3.min.js';
   const script = document.createElement('script');
   script.setAttribute('async', '');
   script.src = src;
--- powerpod/src/js/powerpod.js
+++ powerpod/src/js/powerpod.js
@@ -69,7 +69,7 @@ function setAPI() {
     };
   };
   // @ts-ignore
-  POWERPOD.version = '1.7.2';
+  POWERPOD.version = '1.7.3';
   // @ts-ignore
   window.powerpod = POWERPOD;
 }
