dd06eb4b142e5b03555e89e1148d90f4b942f794
Mihai Listov <mihailistov@gmail.com>
2024-08-12 14:46:00 -0700

    additional fixes

--- powerpod/rollup.config.js
+++ powerpod/rollup.config.js
@@ -5,7 +5,7 @@ import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-import-css';
 
 const licenseContent = `/*!
-* powerpod 1.7.9
+* powerpod 1.8.1
 * https://github.com/bcgov/nr-af-pods/powerpod
 *
 * @license GPLv3 for open source use only
