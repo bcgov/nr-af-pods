79480c1eb5f63702f46b3818bd738e5f7166db1b
Mihai Listov <mihailistov@gmail.com>
2024-08-21 15:10:42 -0700

    TASK 4678: Fix validateNeededFields, rel 2.0.5

--- assets/application/js/application.js
+++ assets/application/js/application.js
@@ -1,6 +1,6 @@
 (function () {
   const src =
-    'https://cdn.jsdelivr.net/gh/bcgov/nr-af-pods@dev/powerpod/releases/powerpod-2.0.4.min.js';
+    'https://cdn.jsdelivr.net/gh/bcgov/nr-af-pods@dev/powerpod/releases/powerpod-2.0.5.min.js';
   const script = document.createElement('script');
   script.setAttribute('async', '');
   script.src = src;
