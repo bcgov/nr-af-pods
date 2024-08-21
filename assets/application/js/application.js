843f1b300ddfdf0d6c0b6caf4ef940843c13d6e4
Mihai <mihailistov@gmail.com>
2024-08-21 01:27:35 -0700

    TASK 4688: Budget step fix for total funding validation (#191)

--- assets/application/js/application.js
+++ assets/application/js/application.js
@@ -1,6 +1,6 @@
 (function () {
   const src =
-    'https://cdn.jsdelivr.net/gh/bcgov/nr-af-pods@dev/powerpod/releases/powerpod-1.9.8.min.js';
+    'https://cdn.jsdelivr.net/gh/bcgov/nr-af-pods@dev/powerpod/releases/powerpod-1.9.9.min.js';
   const script = document.createElement('script');
   script.setAttribute('async', '');
   script.src = src;
--- powerpod/src/js/powerpod.js
+++ powerpod/src/js/powerpod.js
@@ -69,7 +69,7 @@ function setAPI() {
     };
   };
   // @ts-ignore
-  POWERPOD.version = '1.9.8';
+  POWERPOD.version = '1.9.9';
   // @ts-ignore
   window.powerpod = POWERPOD;
 }
