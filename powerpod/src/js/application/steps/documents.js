f898fa2fec42e64808b8b3df05148e9802db90d7
Mihai <mihailistov@gmail.com>
2024-08-28 23:45:19 -0700

    TASK 4709: abpp1 doc step, rel 2.3.3 (#199)
    * TASK 4709: abpp1 doc step
    
    * TASK 4709: abpp1 doc step, rel 2.3.3
    
    * TASK 4709: abpp1 doc step, update abpp json

--- powerpod/src/js/application/steps/documents.js
+++ powerpod/src/js/application/steps/documents.js
@@ -26,50 +26,50 @@ export function customizeDocumentsStep() {
     message: `Configuring documents label for programAbbreviation: ${programAbbreviation}`,
   });
 
-  if (programAbbreviation && programAbbreviation.includes('ABPP')) {
-    let documentsHtml = '';
-    if (programAbbreviation === 'ABPP1') {
-      documentsHtml = `
-        <div>
-          <br><br>
-          <ul>
-            <li>Event/training budget (if not outlined in Deliverables & Budget tab)</li>
-            <li>
-              Verification of the last year of farming income (T2042, T1273, or Schedule 125 - 
-              Farm Revenue) detailing sales by commodity revenue code) or business income 
-              if applying as a food processor
-            </li>
-            <li>Direct Deposit Application (template available on program webpage)</li>
-          </ul>
-        </div>
-      `;
-    }
-    if (programAbbreviation === 'ABPP2') {
-      documentsHtml = `
-        <div>
-          <br><br>
-          <ul>
-            <li>Project budget</li>
-            <li 
-              title="Consultant resume outlining any educational accomplishments and relevant certifications"
-            >
-              Consultant resume
-            </li>
-            <li>Supporting consultant resume (if applicable)</li>
-            <li>Verification of the last year of farming income</li>
-            <li>Direct Deposit Application (template available on program webpage)</li>
-          </ul>
-        </div>
-      `;
-    }
-    const fileUploadLabel = document.querySelector(
-      '#quartech_relatedquotesandplans_label'
-    );
-    logger.info({
-      fn: customizeDocumentsStep,
-      message: `Setting documents label html for fileUploadLabel: ${fileUploadLabel}`,
-    });
-    // @ts-ignore
-    $(fileUploadLabel).after(documentsHtml);
-  }
+  // if (programAbbreviation && programAbbreviation.includes('ABPP')) {
+  //   let documentsHtml = '';
+  //   if (programAbbreviation === 'ABPP1') {
+  //     documentsHtml = `
+  //       <div>
+  //         <br><br>
+  //         <ul>
+  //           <li>Event/training budget (if not outlined in Deliverables & Budget tab)</li>
+  //           <li>
+  //             Verification of the last year of farming income (T2042, T1273, or Schedule 125 - 
+  //             Farm Revenue) detailing sales by commodity revenue code) or business income 
+  //             if applying as a food processor
+  //           </li>
+  //           <li>Direct Deposit Application (template available on program webpage)</li>
+  //         </ul>
+  //       </div>
+  //     `;
+  //   }
+  //   if (programAbbreviation === 'ABPP2') {
+  //     documentsHtml = `
+  //       <div>
+  //         <br><br>
+  //         <ul>
+  //           <li>Project budget</li>
+  //           <li 
+  //             title="Consultant resume outlining any educational accomplishments and relevant certifications"
+  //           >
+  //             Consultant resume
+  //           </li>
+  //           <li>Supporting consultant resume (if applicable)</li>
+  //           <li>Verification of the last year of farming income</li>
+  //           <li>Direct Deposit Application (template available on program webpage)</li>
+  //         </ul>
+  //       </div>
+  //     `;
+  //   }
+  //   const fileUploadLabel = document.querySelector(
+  //     '#quartech_relatedquotesandplans_label'
+  //   );
+  //   logger.info({
+  //     fn: customizeDocumentsStep,
+  //     message: `Setting documents label html for fileUploadLabel: ${fileUploadLabel}`,
+  //   });
+  //   // @ts-ignore
+  //   $(fileUploadLabel).after(documentsHtml);
+  // }
 }
