b7173cb6ab8b377f660405990c4c397764b18daa
idir\lepham <Leo.Pham2@gov.bc.ca>
2024-10-29 15:55:27 -0700

    updated PS scripts for exporting & extracting PODS solutions

--- d365-solutions/export-pods-solutions.ps1
+++ d365-solutions/export-pods-solutions.ps1
@@ -1,11 +1,6 @@
-Connect-CrmOnPremDiscovery -InteractiveMode
-#Export-CrmSolution PODSCommonCDM -Managed
+Connect-CrmOnPremDiscovery -InteractiveModeExport-CrmSolution PODSCommonCDM -Managed
 Export-CrmSolution PODSModelApps -Managed
 Export-CrmSolution PODSPowerPagesPortal -Managed
-#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_managed_1_0_0_98.zip /folder:PODSCommonCDM/managed
+C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_managed_1_0_0_98.zip /folder:PODSCommonCDM/managed
 C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSModelApps_managed_1_0_0_98.zip /folder:PODSModelApps/managed
 C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSPowerPagesPortal_managed_1_0_0_98.zip /folder:PODSPowerPagesPortal/managed
-
-#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_managed.zip /folder:PODSCommonCDM/managed
-#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSModelApps_managed.zip /folder:PODSModelApps/managed
-#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSPowerPagesPortal_managed.zip /folder:PODSPowerPagesPortal/managed
