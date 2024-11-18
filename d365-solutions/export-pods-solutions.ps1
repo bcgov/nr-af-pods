1b8b455c78bb8201adda8427144ffbcebdb1be38
idir\lepham <Leo.Pham2@gov.bc.ca>
2024-11-18 14:03:23 -0800

    Exported Claim Period dependencies

--- d365-solutions/export-pods-solutions.ps1
+++ d365-solutions/export-pods-solutions.ps1
@@ -2,19 +2,21 @@ Connect-CrmOnPremDiscovery -InteractiveMode
 Export-CrmSolution PODSCommonCDM -Managed
 C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_managed_1_0_0_100.zip /folder:PODSCommonCDM/managed
 
+#Export-CrmSolution PODSModelApps -Managed
+#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSModelApps_managed.zip /folder:PODSModelApps/managed
+
+
+#Export-CrmSolution PODSPowerPagesPortal -Managed
+#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSPowerPagesPortal_managed.zip /folder:PODSPowerPagesPortal/managed
+
+
 #Export-CrmSolution PODSCommonCDM
 #C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_1_0_0_100.zip /folder:PODSCommonCDM/Unmanaged
 
 
-Export-CrmSolution PODSModelApps -Managed
-C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSModelApps_managed.zip /folder:PODSModelApps/managed
-
 #Export-CrmSolution PODSModelApps
 #C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSModelApps_1_0_0_100.zip /folder:PODSModelApps/Unmanaged
 
-
-Export-CrmSolution PODSPowerPagesPortal -Managed
-C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSPowerPagesPortal_managed.zip /folder:PODSPowerPagesPortal/managed
 
 #Export-CrmSolution PODSPowerPagesPortal
 #C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSPowerPagesPortal_1_0_0_100.zip /folder:PODSPowerPagesPortal/Unmanaged
