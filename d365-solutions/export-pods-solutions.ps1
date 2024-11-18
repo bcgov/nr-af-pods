377b1ede04a7b6898b4eae726647fa85391a1c8a
idir\lepham <Leo.Pham2@gov.bc.ca>
2024-11-18 13:41:30 -0800

    sprint 44 fixes for TEST Deployment

--- d365-solutions/export-pods-solutions.ps1
+++ d365-solutions/export-pods-solutions.ps1
@@ -1,20 +1,20 @@
 Connect-CrmOnPremDiscovery -InteractiveMode
 Export-CrmSolution PODSCommonCDM -Managed
-C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_managed_1_0_0_99.zip /folder:PODSCommonCDM/managed
+C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_managed_1_0_0_100.zip /folder:PODSCommonCDM/managed
 
-Export-CrmSolution PODSCommonCDM -Unmanaged
-C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_1_0_0_99.zip /folder:PODSCommonCDM/Unmanaged
+#Export-CrmSolution PODSCommonCDM
+#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSCommonCDM_1_0_0_100.zip /folder:PODSCommonCDM/Unmanaged
 
 
 Export-CrmSolution PODSModelApps -Managed
 C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSModelApps_managed.zip /folder:PODSModelApps/managed
 
-Export-CrmSolution PODSModelApps -Unmanaged
-C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSModelApps_1_0_0_99.zip /folder:PODSModelApps/Unmanaged
+#Export-CrmSolution PODSModelApps
+#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSModelApps_1_0_0_100.zip /folder:PODSModelApps/Unmanaged
 
 
 Export-CrmSolution PODSPowerPagesPortal -Managed
 C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSPowerPagesPortal_managed.zip /folder:PODSPowerPagesPortal/managed
 
-Export-CrmSolution PODSPowerPagesPortal -Unmanaged
-C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSPowerPagesPortal_1_0_0_99.zip /folder:PODSPowerPagesPortal/Unmanaged
+#Export-CrmSolution PODSPowerPagesPortal
+#C:\Projects\DevOps\D365Tools\coretools\SolutionPackager /action:Extract /zipfile:PODSPowerPagesPortal_1_0_0_100.zip /folder:PODSPowerPagesPortal/Unmanaged
