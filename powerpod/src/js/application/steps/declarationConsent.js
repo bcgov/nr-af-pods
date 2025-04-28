9fec9dab6970c592034b40adec7546ae400a0b21
Mihai Listov <mihailistov@gmail.com>
2025-04-28 16:39:11 -0700

    TASK XXXX: Fix TFCCRF consent form json data

--- powerpod/src/js/application/steps/declarationConsent.js
+++ powerpod/src/js/application/steps/declarationConsent.js
@@ -12,7 +12,7 @@ import {
   hideFieldsAndSections,
 } from '../../common/html.js';
 import { Logger } from '../../common/logger.js';
-import { getProgramAbbreviation } from '../../common/program.ts';
+import { getProgramAbbreviation, getProgramData } from '../../common/program.ts';
 import { hidePageDescription } from '../../common/sections.js';
 import store from '../../store/index.js';
 
@@ -146,6 +146,71 @@ function styleSignatureDivs(retries = 10, delay = 500) {
     document.head.appendChild(style);
   } catch (e) {
     console.error('Error styling signature buttons:', e);
+  }
+}
+
+export function getConsentText() {
+  const programAbbreviation = getProgramAbbreviation();
+  const programName = getProgramData()?.quartech_applicantportalprogramname;
+  const programNameTag = '%%ProgramName%%';
+  let htmlConsent = '';
+  if (programAbbreviation === 'TFCCRF') {
+    htmlConsent = `<div style='font-style: italic;'>
+      <span>BY SUBMITTING THIS APPLICATION FORM TO %%ProgramName%% (the "Fund"), I:</span>
+      <u style='text-decoration:none;'>
+          <li>represent that I am the applicant or the fully authorized signatory of the applicant;</li>
+          <li>
+              declare that I have/the applicant has not knowingly submitted false or misleading information and that the information provided in this application and attachments are true and correct in every respect to the best of my/the applicant's knowledge;</li>
+          <li>acknowledge the information provided on this application form and attachments will be used by the Fund Administrators to assess the applicant's eligibility for payment from the Fund;</li>
+          <li>understand that failing to comply with all application requirements may delay the processing of this application or make the applicant ineligible to receive payment under the Fund;</li>
+          <li>represent that I have/the applicant has read and understood the Terms and Conditions and agree(s) to be bound by the Terms and Conditions;</li>
+          <li>represent that the applicant is in compliance with all Fund eligibility requirements as described in the Terms and Conditions, and in this document;</li>
+          <li>agree to proactively disclose any partners within the same organization or farming/food processing operation who receive support from this Fund.</li>        
+          <li>acknowledge that the Business Number (GST Number) is collected by the Ministry and/or a contractor retained by the Ministry to administer the Fund under the authority of the Income Tax Act for the purpose of reporting income.</li>
+          <li>agree for B.C. Investment Agriculture Foundation (IAF) to make all payments by direct deposit or mailed cheque with the banking information provided in this application;</li>
+          <li>expressly authorizes the Fund Administrator to obtain information from any provincial or federal government department, agency or third Party for the purposes of verifying the contents of theApplication or Tree Fruit Inventory or any other information provided by the Applicant under this Fund.</li>
+          <li>where the Applicant’s Tree Fruit Inventory includes apples, consent to the Ministry to disclose 2024 Production Insurance Tree Fruit Policy purchase; contact information; apple acreage; and, where applicable, any and all of the Applicant’s Production Insurance records for the crop year.  This disclosure is for the purpose of Inner Harbour Consulting Inc. contacting the Applicant regarding participation in a vote on the establishment of a BC Apple Marketing Commission. This consent is effective as of the date this application form is submitted.</li>
+      </u>
+      <br/>
+  </div>`;
+    htmlConsent = htmlConsent.replace(programNameTag, programName);
+    return htmlConsent;
+  } else if (programAbbreviation === 'TFCR') {
+    htmlConsent = `<div style='font-style: italic;'>
+      <span>BY SUBMITTING THIS APPLICATION FORM TO %%ProgramName%% (the "Program"), I:</span>
+      <u style='text-decoration:none;'>
+          <li>represent that I am the applicant or the fully authorized signatory of the applicant;</li>
+          <li>
+              declare that I have/the applicant has not knowingly submitted false or misleading information and that the information provided in this application and attachments are true and correct in every respect to the best of my/the applicant's knowledge;</li>
+          <li>acknowledge the information provided on this application form and attachments will be used by program staff to assess the applicant's eligibility for funding from the Program;</li>
+          <li>understand that failing to comply with all application requirements may delay the processing of this application or make the applicant ineligible to receive funding under the Program;</li>
+          <li>represent that I have/the applicant has read and understood the Program Guide and agree(s) to be bound by the Program Guide;</li>
+          <li>represent that the applicant is in compliance with all Program eligibility requirements as described in the Program Guide, and in this document;</li>
+          <li>agree to proactively disclose to the Program all other sources of funding the applicant or any partners within the same organization or the same farming or food processing operation receives with respect to the projects funded by this Program, including financial and/or in-kind contributions from federal, provincial, or municipal government;</li>        
+          <li>acknowledge that the Business Number (GST Number) is collected by the Ministry and/or a contractor retained by the Ministry to administer the Program under the authority of the Income Tax Act for the purpose of reporting income.</li>
+      </u>
+      <br/>
+  </div>`;
+    htmlConsent = htmlConsent.replace(programNameTag, programName);
+    return htmlConsent;
+  } else {
+    let htmlConsent = `<div style='font-style: italic;'>
+      <span>BY SUBMITTING THIS APPLICATION FORM TO %%ProgramName%% (the "Program"), I:</span>
+      <u style='text-decoration:none;'>
+          <li>represent that I am the applicant or the fully authorized signatory of the applicant;</li>
+          <li>
+              declare that I have/the applicant has not knowingly submitted false or misleading information and that the information provided in this application and attachments is true and correct in every respect to the best of my/the applicant's knowledge;</li>
+          <li>acknowledge the information provided on this application form and attachments will be used by the Ministry of Agriculture and Food (the "Ministry") to assess the applicant's eligibility for funding from the Program;</li>
+          <li>understand that failing to comply with all application requirements may delay the processing of this application or make the applicant ineligible to receive funding under the Program;</li>
+          <li>represent that I have/the applicant has read and understood the Program Terms and Conditions and agree(s) to be bound by the Program Terms and Conditions;</li>
+          <li>represent that the applicant is in compliance with all Program eligibility requirements as described in the Program Terms and Conditions, and in this document;</li>
+          <li>agree to proactively disclose to the Program all other sources of funding the applicant or any partners within the same organization or the same farming or food processing operation receives with respect to the projects funded by this Program, including financial and/or in-kind contributions from federal, provincial, or municipal government;</li>        
+          <li>acknowledge that the Business Number (GST Number) is collected by the Ministry under the authority of the Income Tax Act for the purpose of reporting income.</li>
+      </u>
+      <br/>
+  </div>`;
+    htmlConsent = htmlConsent.replace(programNameTag, programName);
+    return htmlConsent;
   }
 }
 
--- powerpod/src/js/powerpod.js
+++ powerpod/src/js/powerpod.js
@@ -92,7 +92,7 @@ function setAPI() {
     };
   };
   // @ts-ignore
-  POWERPOD.version = '3.9.5';
+  POWERPOD.version = '3.9.6';
   // @ts-ignore
   window.powerpod = POWERPOD;
 }
